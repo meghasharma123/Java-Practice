3f98c1a35c21ff2001ad059af5f98a37ccabf7a0
meghasharma123 <meghasharma2822000@gmail.com>
2023-01-30 22:01:14 +0530

    first

---
+++ Practice/Demo.java
@@ -0,0 +1,10 @@
+public class Demo{
+    public static void main(String args[]){
+        int i=0;
+        do{
+            System.out.println("Hello "+i);
+            i++;
+        }while(i<0);
+        System.out.println("Bye "+i);
+    }
+}
---
+++ Practice/OfficeTiming.java
@@ -0,0 +1,10 @@
+public class OfficeTiming {
+    public static void main(String args[]) {
+        for(int i=0;i<5;i++){
+            System.out.println("Days : "+(i+1));
+            for(int j=1;j<9;j++){
+                System.out.println(" "+(j+9)+"-"+(j+10));
+            }
+        }
+    }
+}
